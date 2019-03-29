11a1a61c4dfaab50bccf0982bb6cebfb127ae152
wanghao4 <Wh996399>
2019-03-29 15:35:36 +0800

    增加统一异常处理控制器

--- src/main/java/com/wh/web/myweb/constants/ResultConstant.java
+++ src/main/java/com/wh/web/myweb/constants/ResultConstant.java
@@ -14,6 +14,16 @@ public class ResultConstant {
     public static final Result FAIL = new Result("500", "处理失败");
 
     /**
+     * 默认异常返回
+     */
+    public static final Result EXCEPTION = new Result("555", "服务异常");
+
+    /**
+     * 默认身份验证不通过返回
+     */
+    public static final Result LOGIN_EXCEPTION = new Result("400", "用户身份信息验证不通过");
+
+    /**
      * 自定义成功返回数据
      */
     public static Result newSuccess(Object data) {
